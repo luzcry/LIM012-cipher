acc6f40300756998eca9288c91c5de28825fd317
luzcry <58492755+luzcry@users.noreply.github.com>
2020-02-14 00:33:50 -0500

    agregué la funcion encode y funciona!!

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,24 @@
+
+const encode = (text, n) => {
+    let offset = n.value
+    let result = [];
+    let array = text.value;
+    for (let index = 0; index < array.length; index++) {
+        let charCode = array.charCodeAt(index)
+        let newChar = String.fromCharCode((charCode-65+offset) % 26 + 65)
+        result.push(newChar);
+    }
+    console.log("result", result, result.toString())
+    return result
+}
+const decode = (texto) => {
+    return texto
+} 
+
 const cipher = {
-  
-};
+    encode,
+    decode
+  };
+
 export default cipher;
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import cipher from './cipher.js';
 
+/*const fixedStr =  ["D", "E", "F","G","H","I","J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C"];
+const normalStr = ["A", "B", "C", "D", "E", "F", "G", "H","I","J","K","L","M","N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let offset = 0; */
+//let char = 0;
+let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let message = document.querySelector("#texto1");
+//let cadena = 0;
+//codigo ascii
+function convertText () {
+  let n = str.charCodeAt(); 
+  document.querySelector("#section1").innerHTML =  (`Tu texto es ${n}`);
+}
+convertText();
+
 let offsetCypher = document.querySelector("#offset1");
-console.log(offsetCypher);
 
-let Message = document.querySelector("#texto1");
-console.log(Message);
 
-let offsetDecode = document.querySelector("#offset2");
-console.log()
+//let offsetDecode = document.querySelector("#offset2");
 
-let Message2 = document.querySelector("#text2");
+//let Message2 = document.querySelector("#text2");
 
 const btn = document.querySelector("#btn");
 btn.addEventListener('click', () => {
-  alert(`Hola! Mucho gusto en conocerte`);
+  document.querySelector("#section1").innerHTML = (`Tu texto es ${cipher.encode(message, offsetCypher)}`);
 });
 
 const btn2 = document.querySelector("#btn2");
 btn2.addEventListener('click', () => {
-  alert(`Hola! Mucho gusto en conocerte`);
+  document.querySelector("#section2").innerHTML = (`Tu texto es ${message}`);
 });
 
-console.log(cipher);
