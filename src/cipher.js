18b1d99025430c54d22195d7206b5247b3bd4a5d
luzcry <58492755+luzcry@users.noreply.github.com>
2020-02-18 12:48:25 -0500

    modifique el css flex para grid de nuevo pero usando la propiedad minmax() caracteres especiales parece que funcionan e n funtion encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,39 @@
-
-export const encode = (text, n) => {
-
+ const encode = (text, n) => {
     let myStringCipher = '';  
     let cipherFormula;
     for (let index = 0; index < text.length; index++) {
         let charCode = text.charCodeAt(index)
-        console.log("n", n);
-        if (charCode >= 65 && charCode <= 90){
+        if (charCode >= 65 && charCode <= 90){ //mayusculas (funciona)
             cipherFormula = (charCode - 65 + n) % 26 + 65;
-            console.log("formula", cipherFormula, n)
             let newChar =  String.fromCharCode(cipherFormula);
             myStringCipher += newChar;
-        } else if(charCode === 32) {
+
+        } else if(charCode === 32) { //colocar espacios
             myStringCipher += ' ';
-            
-        } else if (charCode >= 97 && charCode <=122) {
+
+        } else if(charCode >= 164 && charCode<=165) { //Ñ y ñ (no funciona)
+        cipherFormula = (charCode - 164 + n) % 26 + 164;
+        let newChar = String.fromCharCode(cipherFormula);
+        myStringCipher += newChar;
+
+        } else if (charCode >= 97 && charCode <=122) { //minusculas (funciona)
             cipherFormula = (charCode - 97 + n) % 26 + 97;
             let newChar =  String.fromCharCode(cipherFormula);
              myStringCipher += newChar;
-        } else if (charCode === 164 && charCode === 165) {
-            cipherFormula = (charCode - 164 + n) % 26 + 164;
-            let newChar = String.fromCharCode(cipherFormula)
+      
+        } else if (charCode >= 33 && charCode <=47) { //caracteres especiales (funciona)
+            cipherFormula = (charCode - 33 + n) % 26 + 33;
+            let newChar = String.fromCharCode(cipherFormula);
             myStringCipher += newChar;
-            console.log  ("prueba de ñ", cipherFormula, n)
+
+        } else{
+         break;
         }
     }
     return myStringCipher;       
 }
 
-export const decode = (text, n) => {
+ const decode = (text, n) => {
     let myDecode = '';
     let decodeFormula;
     for (let index = 0; index < text.length; index++) {
@@ -36,7 +41,7 @@ export const decode = (text, n) => {
         if (charCode >= 65 && charCode <= 90){
             decodeFormula = (charCode - 13 - n) % 26 + 65;
             console.log("formula", decodeFormula, n);
-            let newChar = String.fromCharCode(decodeFormula)
+            let newChar = String.fromCharCode(decodeFormula);
             myDecode += newChar;
         }
         else if (charCode === 32) {
@@ -45,11 +50,17 @@ export const decode = (text, n) => {
             decodeFormula = (charCode - 45 - n) % 26 + 97;
             let newChar =  String.fromCharCode(decodeFormula);
             myDecode += newChar;
-        } else if (charCode === 164 && charCode === 165) {
-            decodeFormula = (charCode -13 - n) % 26 + 164;
+        } else if (charCode >= 33 && charCode <=47) { //caracteres especiales (no funciona)
+            decodeFormula = (charCode - 13 - n) % 26 + 33;
+            let newChar = String.fromCharCode(decodeFormula);
+            myDecode += newChar;
+        
+        }else if (charCode === 164) {
+            decodeFormula = (charCode - 164 - n) % 26 + 164;
             let newChar = String.fromCharCode(decodeFormula)
             myDecode += newChar;
-            
+        } else {
+            break;
         }
     }
     return myDecode;   
@@ -59,7 +70,7 @@ const cipher = {
     encode,
     decode
   };
-
+  
 export default cipher;
 
 
