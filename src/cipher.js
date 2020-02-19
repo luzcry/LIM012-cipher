226cda5b38b0ea4448df3c5a1297d871374f19e9
luzcry <58492755+luzcry@users.noreply.github.com>
2020-02-18 21:17:13 -0500

    test 2 failed, 7 passed, 9 total

--- src/cipher.js
+++ src/cipher.js
@@ -1,77 +1,70 @@
- const encode = (text, n) => {
-    let myStringCipher = '';  
-    let cipherFormula;
-    for (let index = 0; index < text.length; index++) {
-        let charCode = text.charCodeAt(index)
-        if (charCode >= 65 && charCode <= 90){ //mayusculas (funciona)
-            cipherFormula = (charCode - 65 + n) % 26 + 65;
-            let newChar =  String.fromCharCode(cipherFormula);
-            myStringCipher += newChar;
+ const encode = (n, text) => {
+   let myStringCipher = '';
+   let cipherFormula;
+   try {
+     for (let index = 0; index < text.length; index++) {
+       let charCode = text.charCodeAt(index)
+       if (charCode >= 65 && charCode <= 90) { //mayusculas (funciona)
+         cipherFormula = (charCode - 65 + n) % 26 + 65;
+         let newChar = String.fromCharCode(cipherFormula);
+         myStringCipher += newChar;
 
-        } else if(charCode === 32) { //colocar espacios
-            myStringCipher += ' ';
+       } else if (charCode === 32) { //colocar espacios
+         myStringCipher += ' ';
 
-        } else if(charCode >= 164 && charCode<=165) { //Ñ y ñ (no funciona)
-        cipherFormula = (charCode - 164 + n) % 26 + 164;
-        let newChar = String.fromCharCode(cipherFormula);
-        myStringCipher += newChar;
+       } else if (charCode >= 164 && charCode <= 165) { //Ñ y ñ (no funciona)
+         cipherFormula = (charCode - 164 + n) % 26 + 164;
+         let newChar = String.fromCharCode(cipherFormula);
+         myStringCipher += newChar;
 
-        } else if (charCode >= 97 && charCode <=122) { //minusculas (funciona)
-            cipherFormula = (charCode - 97 + n) % 26 + 97;
-            let newChar =  String.fromCharCode(cipherFormula);
-             myStringCipher += newChar;
-      
-        } else if (charCode >= 33 && charCode <=47) { //caracteres especiales (funciona)
-            cipherFormula = (charCode - 33 + n) % 26 + 33;
-            let newChar = String.fromCharCode(cipherFormula);
-            myStringCipher += newChar;
+       } else if (charCode >= 97 && charCode <= 122) { //minusculas (funciona)
+         cipherFormula = (charCode - 97 + n) % 26 + 97;
+         let newChar = String.fromCharCode(cipherFormula);
+         myStringCipher += newChar;
 
-        } else{
-         break;
-        }
-    }
-    return myStringCipher;       
-}
+       } else if (charCode >= 33 && charCode <= 47 || charCode >= 58 && charCode <= 64) { //caracteres especiales (funciona)
+         let newChar = String.fromCharCode(charCode);
+         myStringCipher += newChar;
 
- const decode = (text, n) => {
-    let myDecode = '';
-    let decodeFormula;
-    for (let index = 0; index < text.length; index++) {
-        let charCode = text.charCodeAt(index)
-        if (charCode >= 65 && charCode <= 90){
-            decodeFormula = (charCode - 13 - n) % 26 + 65;
-            console.log("formula", decodeFormula, n);
-            let newChar = String.fromCharCode(decodeFormula);
-            myDecode += newChar;
-        }
-        else if (charCode === 32) {
-            myDecode += ' ';
-        } else if  (charCode >= 97 && charCode <=122) {
-            decodeFormula = (charCode - 45 - n) % 26 + 97;
-            let newChar =  String.fromCharCode(decodeFormula);
-            myDecode += newChar;
-        } else if (charCode >= 33 && charCode <=47) { //caracteres especiales (no funciona)
-            decodeFormula = (charCode - 13 - n) % 26 + 33;
-            let newChar = String.fromCharCode(decodeFormula);
-            myDecode += newChar;
-        
-        }else if (charCode === 164) {
-            decodeFormula = (charCode - 164 - n) % 26 + 164;
-            let newChar = String.fromCharCode(decodeFormula)
-            myDecode += newChar;
-        } else {
-            break;
-        }
-    }
-    return myDecode;   
-}
+       }
+     }
+   } catch (error) {
+     return error;
+   }
 
-const cipher = {
-    encode,
-    decode
-  };
-  
-export default cipher;
+   return myStringCipher;
+ }
 
+ const decode = (n, text) => {
+   let myDecode = '';
+   let decodeFormula;
+   for (let index = 0; index < text.length; index++) {
+     let charCode = text.charCodeAt(index)
+     if (charCode >= 65 && charCode <= 90) {
+       decodeFormula = (charCode - 13 - n) % 26 + 65;
+       let newChar = String.fromCharCode(decodeFormula);
+       myDecode += newChar;
+     } else if (charCode === 32) {
+       myDecode += ' ';
+     } else if (charCode >= 97 && charCode <= 122) {
+       decodeFormula = (charCode - 45 - n) % 26 + 97;
+       let newChar = String.fromCharCode(decodeFormula);
+       myDecode += newChar;
+     } else if (charCode >= 33 && charCode <= 47 || charCode >= 58 && charCode <= 64) { //caracteres especiales
+       let newChar = String.fromCharCode(charCode);
+       myDecode += newChar;
+     } else if (charCode === 164) {
+       decodeFormula = (charCode - 164 - n) % 26 + 164;
+       let newChar = String.fromCharCode(decodeFormula)
+       myDecode += newChar;
+     }
+   }
+   return myDecode;
+ }
 
+ const cipher = {
+   encode,
+   decode
+ };
 
+ export default cipher;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ const btn = document.querySelector("#btn");
 
 btn.addEventListener('click', () => {
   let message = document.querySelector("#text1").value;
-  let offsetCypher =  parseInt(document.getElementById('offset1').value, 10)
-  document.querySelector("#section1").innerHTML = (` ${cipher.encode(message, offsetCypher)}`);
+  let offsetCypher = parseInt(document.getElementById('offset1').value, 10)
+  document.querySelector("#section1").innerHTML = (` ${cipher.encode( offsetCypher,message)}`);
 });
 
 const btn2 = document.querySelector("#btn2");
 btn2.addEventListener('click', () => {
   let Message2 = document.querySelector("#text2").value;
-  let offset =  parseInt(document.getElementById('offset2').value, 10)
-  document.querySelector("#section2").innerHTML = (` ${cipher.decode(Message2, offset)}`);
+  let offset = parseInt(document.getElementById('offset2').value, 10)
+  document.querySelector("#section2").innerHTML = (` ${cipher.decode(offset, Message2 )}`);
 });
-
