77fa9c6fd94506b7890f429260b924b55f3f3340
luzcry <58492755+luzcry@users.noreply.github.com>
2020-02-21 08:34:59 -0500

    modifiqué el readme

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ feedback recibido: <br>
 -Al primer prototipo le faltaba el valor "offset" (coach) <br>
 -Cambiar el orden del input y las instrucciones (coach) <br>
 -Adaptar el diseño a la web (compañera) <br>
--Investigar sobre propiedades flex wrap y minmax para adaptar la página. (compañera) <br>
+-Investigar sobre propiedades flex wrap y minmax para adaptar la página.(compañera) <br>
+-Usar throw new error en vez de return error (coach) <br>
+-Modificar el Try que estaba envolviendo mis funciones y colocar condicionales para los casos de error en el test(coach)<br>
 
 Prototipo de baja fidelidad hecho en papel <br>
 
@@ -15,10 +17,6 @@ Prototipo de baja fidelidad hecho en papel <br>
 Prototipo hecho en inVision <br>
 
 ![caeser-cipher](https://luzcry.github.io/LIM012-cipher/src/iconos/prototipo2.png)
-
-Imagen final del proyecto <br>
-
-![caeser-cipher](https://luzcry.github.io/LIM012-cipher/src/iconos/FINAL.png)
 
 Sprint CheckOut: <br>
 En el primer sprint hice un prototipo en invision que traduje en HTML usando selectores de CSS. En mi index.js coloqué la funcionalidad de los botones y definí que elementos irian en la ventana de resultados. En cipher.js definí las constantes encode y decode, consta de las variables que las componen (encode: myStringCipher, cipherFormula, newChar; decode: myDecode, decodeFormula, newChar), un bucle for para recorrer el arreglo, una variable para captar el codigo ASCII, aplicarle una formula, devolver el número del caracter cifrado para insertar todo en la variable de resultados. Al momento funciona un cifrado en letras mayúsculas y una condicional if que reconoce si el usuario coloca espacios en el texto.
@@ -36,3 +34,11 @@ Investigación UX: <br>
 Explicar quiénes son los usuarios y los objetivos en relación con el producto. <br>
 Explicar cómo el producto soluciona los problemas/necesidades de dichos usuarios. <br>
 Los usuarios son personas de 12 años en adelante, que se interesan en tener conocimientos de cultura general mediante la lectura de artículos en una revista digital que fomenta la ciencia entre los mas jóvenes, con motivo del mes del amor y la amistad esta revista quería implementar en su web un juego que les mostrara de manera didáctica uno de los cifrados mas sencillos y antiguos y que, por medio de los botones de compartir, se pudiera captar nuevos lectores del contenido de la pagina.
+
+Imagen final del proyecto <br>
+
+![caeser-cipher](https://luzcry.github.io/LIM012-cipher/src/iconos/FINAL.png) <br>
+
+Test realizados el 21/02/2020 <br>
+![caeser-cipher](https://luzcry.github.io/LIM012-cipher/src/iconos/wii1.png) <br>
+![caeser-cipher](https://luzcry.github.io/LIM012-cipher/src/iconos/wii2.png)
--- src/cipher.js
+++ src/cipher.js
@@ -5,17 +5,12 @@ const encode = (n, text) => {
   for (let index = 0; index < text.length; index++) {
     let charCode = text.charCodeAt(index);
     if (charCode >= 65 && charCode <= 90) {
-      //mayusculas (funciona)
+      //mayusculas
       cipherFormula = ((charCode - 65 + n) % 26) + 65;
       let newChar = String.fromCharCode(cipherFormula);
       myStringCipher += newChar;
-    } else if (charCode >= 164 && charCode <= 165) {
-      //Ñ y ñ (no funciona)
-      cipherFormula = ((charCode - 164 + n) % 26) + 164;
-      let newChar = String.fromCharCode(cipherFormula);
-      myStringCipher += newChar;
     } else if (charCode >= 97 && charCode <= 122) {
-      //minusculas (funciona)
+      //minusculas
       cipherFormula = ((charCode - 97 + n) % 26) + 97;
       let newChar = String.fromCharCode(cipherFormula);
       myStringCipher += newChar;
@@ -23,20 +18,17 @@ const encode = (n, text) => {
       (charCode >= 32 && charCode <= 47) ||
       (charCode >= 58 && charCode <= 64)
     ) {
-      //caracteres especiales (funciona)
+      //caracteres especiales
       let newChar = String.fromCharCode(charCode);
       myStringCipher += newChar;
     }
+    //typeError
   }
   try {
     if (n == " ") throw error;
     if (n == "0" && text == "0") throw error;
     if (n == null) throw error;
     if (encode == 0) throw error;
-    /* expect(() => cipher.encode()).toThrow(TypeError);
-           expect(() => cipher.encode(0)).toThrow(TypeError);
-           expect(() => cipher.encode(null, [])).toThrow(TypeError);
-           expect(() => cipher.encode(0, 0)).toThrow(TypeError);*/
   } catch (error) {
     throw new error();
   }
@@ -50,27 +42,26 @@ const decode = (n, text) => {
   let error = TypeError;
   for (let index = 0; index < text.length; index++) {
     let charCode = text.charCodeAt(index);
+    //mayusculas
     if (charCode >= 65 && charCode <= 90) {
       decodeFormula = ((charCode - 13 - n) % 26) + 65;
       let newChar = String.fromCharCode(decodeFormula);
       myDecode += newChar;
+      //minusculas
     } else if (charCode >= 97 && charCode <= 122) {
       decodeFormula = ((charCode - 45 - n) % 26) + 97;
       let newChar = String.fromCharCode(decodeFormula);
       myDecode += newChar;
+      //caracteres especiales
     } else if (
       (charCode >= 32 && charCode <= 47) ||
       (charCode >= 58 && charCode <= 64)
     ) {
-      //caracteres especiales
       let newChar = String.fromCharCode(charCode);
-      myDecode += newChar;
-    } else if (charCode === 164) {
-      decodeFormula = ((charCode - 164 - n) % 26) + 164;
-      let newChar = String.fromCharCode(decodeFormula);
       myDecode += newChar;
     }
   }
+  //typeError
   try {
     if (text == null) throw error;
     if (text == "0") throw error;
